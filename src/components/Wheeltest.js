94e9fe95213e74da97495f7a5d54f9ed61504988
hamlitary <lemonrain@naver.com>
2022-06-28 03:40:10 +0900

    Add Some CSS and modal

--- src/components/Wheeltest.js
+++ src/components/Wheeltest.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
-import { Wheel } from 'react-custom-roulette'
+import React, { useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+import { makeStyles, Modal } from "@material-ui/core";
 
 const data = [
   { option: '0' },
   { option: '1' },
   { option: '2' },
+  { option: '3' },
+  { option: '4' },
+  { option: '5' },
 ]
 
 const Wheeltest = () => {
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleSpinClick = () => {
     const newPrizeNumber = Math.floor(Math.random() * data.length)
@@ -23,12 +36,20 @@ const Wheeltest = () => {
         mustStartSpinning={mustSpin}
         prizeNumber={prizeNumber}
         data={data}
-
+        backgroundColors={["#5500FF", "#ffffff"]}
+        textColors={["#ffffff", "#5500FF"]}
         onStopSpinning={() => {
           setMustSpin(false);
+          handleOpen();
         }}
       />
       <button onClick={handleSpinClick}>SPIN</button>
+      <Modal open={open} onClose={handleClose} className="Ccelebnodal">
+        <div className="Cceleb">
+          <h5>당첨을 축하합니다!</h5>
+          <p>{data[prizeNumber].option}</p>
+        </div>
+      </Modal>
     </>
   )
 }
